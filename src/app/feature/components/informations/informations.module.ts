32aa3315f95857ae57039f90c3c17fba13d20304
Nomera67 <y.rechtenstein@gmail.com>
2023-04-13 17:53:45 +0200

    add the HTML structure of about section and little bit of SCSS

--- src/app/feature/components/informations/informations.module.ts
+++ src/app/feature/components/informations/informations.module.ts
@@ -3,11 +3,19 @@ import { CommonModule } from '@angular/common';
 
 import { InformationsRoutingModule } from './informations-routing.module';
 import { InformationsComponent } from './informations.component';
+import { AuthorComponent } from './author/author.component';
+import { MuseumComponent } from './museum/museum.component';
+import { WebsiteComponent } from './website/website.component';
+import { IntroductionComponent } from './introduction/introduction.component';
 
 
 @NgModule({
   declarations: [
-    InformationsComponent
+    InformationsComponent,
+    AuthorComponent,
+    MuseumComponent,
+    WebsiteComponent,
+    IntroductionComponent
   ],
   imports: [
     CommonModule,
